6460f6c2e5f1a2ee9cef868f8affd8bf03eb45a7
james-e-thomas <65831213+james-e-thomas@users.noreply.github.com>
2021-01-18 10:59:45 +0000

    Create JSONObjectManifest.js

---
+++ JSONObjectManifest.js
@@ -0,0 +1,19 @@
+var myDynamicManifest = {
+  "name": "fplGraphed",
+  "short_name": "fplGraphed",
+  "start_url": "<your-url>",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "description": "Visualise your FPL mini-leagues",
+  "theme_color": "#2e3842",
+  "icons": [
+    {
+      "src": "images/icon.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
+const stringManifest = JSON.stringify(myDynamicManifest);
+const blob = new Blob([stringManifest], {type: 'application/json'});
+const manifestURL = URL.createObjectURL(blob);
